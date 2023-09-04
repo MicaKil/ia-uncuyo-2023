0e21766351b375056f162a1499eb0434de14eee1
micakil <micaeladellongo@gmail.com>
2023-09-04 19:58:58 -0300

    update tp3

--- tp3-busquedas-no-informadas/code/env.py
+++ tp3-busquedas-no-informadas/code/env.py
@@ -8,9 +8,28 @@
 # entorno completamente observable, determinista y estático.
 
 from random import randint, random
+from math import floor
 import numpy as np
 
 class Environment:
     def __init__(self):
-        matriz = np.full((100, 100), '_', dtype=str)
-        #print(matriz)
+        self.size = 100
+        self.maze = np.full((self.size, self.size), '_', dtype=str)
+        self.obstacle_prob = 0.08 #8% de prob
+        self.obstacles = floor(self.size**2 * 0.08) #calc de núm de obstáculos
+        self.gen_maze()
+
+        #print(self.maze)
+    
+    # si está vacía la coor (x, y)
+    def is_empty(self, x: int, y: int):
+        return self.maze[x][y] == '_'
+    
+    #genera el laberinto 
+    def gen_maze(self):
+        for i in range(self.obstacles):
+            (x, y) = (randint(0, self.size - 1), randint(0, self.size - 1)) 
+            if self.is_empty(x, y):
+                self.maze[x][y] = 'X'
+            else:
+                (x, y) = (randint(0, self.size - 1), randint(0, self.size - 1))
