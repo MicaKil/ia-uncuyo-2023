951750770b61d51118cfa7e592306e98f2a5a0c7
micakil <micaeladellongo@gmail.com>
2023-09-18 11:40:21 -0300

    init tp4

---
+++ tp4-busquedas-informadas/code/a_start_agent.py
@@ -0,0 +1,17 @@
+# Implementar un agente basado en objetivos que dado un punto de inicio y
+# un punto destino, encuentre el camino óptimo.
+
+# Considerar lo siguiente:
+
+# 1. El entorno del agente está compuesto por una grilla de 100x100 en donde los
+# obstáculos se generan al azar. Se trata de un entorno completamente observable,
+# determinista y estático.
+# 2. Las acciones posibles del agente son: (arriba, abajo, izquierda, derecha)
+# 3. El agente deberá ser capaz de resolver el problema planteado mediante un algoritmo de
+# búsqueda A*.
+# 4. Proponer una heurística admisible y consistente para el problema.
+# 5. Al finalizar el proceso de formulación se deberán imprimir por pantalla:
+    # a. La matriz generada con los obstáculos
+    # b. La secuencia de estados completa para llegar desde el estado inicial al estado
+    # destino. (si es posible)
+
